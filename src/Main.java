bccadecafed62c27f491d0eca41fd6282cd8eb48
liamdono <donovali@mail.uc.edu>
2022-09-22 15:05:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+        int birthMonth = 12;
+
+        if(birthMonth >= 12 && birthMonth <=1)
+        {
+            System.out.println("Your birth month is" + birthMonth);
+        }
+        else
+        {
+            System.out.println("Your birth month is incorrect!");
+        }
+    }
+}
